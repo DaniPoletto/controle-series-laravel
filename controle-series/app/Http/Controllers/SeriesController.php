d54679fb6077cd984111d5b6978e1ec95e70b9f8
Daniela Poletto <danielapoletto_bep@hotmail.com>
2022-07-20 20:29:12 -0300

    flash messages

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -11,6 +11,10 @@ class SeriesController extends Controller
    public function index(Request $request)
    {
       $series = Serie::query()->orderBy('nome')->get();
+      $mensagemSucesso = session('mensagem.sucesso');
+      // $mensagemSucesso = $request->session()->get('mensagem.sucesso');
+      // $request->session()->forget('mensagem.sucesso');
+
       // $series = Serie::all();
       
       // $series = DB::select('SELECT nome FROM series;');
@@ -32,7 +36,7 @@ class SeriesController extends Controller
       // return view('listar-series', compact('series'));
 
       //é o mesmo que 
-      return view('series.index')->with('series', $series);
+      return view('series.index')->with('series', $series)->with('mensagemSucesso', $mensagemSucesso);
    }
 
    public function create()
@@ -42,7 +46,9 @@ class SeriesController extends Controller
 
    public function store(Request $request) 
    {
-      Serie::create($request->all());
+      $serie = Serie::create($request->all());
+      // session(['mensagem.sucesso' => 'Série adicionada com sucesso']);
+      // $request->session()->flash('mensagem.sucesso',"Série '{$serie->nome}' adicionada com sucesso");
 
       // $nomeSerie = $request->input('nome');
 
@@ -51,11 +57,17 @@ class SeriesController extends Controller
       // $serie->save();
 
       //DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie]); 
-      return redirect()->route('series.index');
+      return redirect()
+               ->route('series.index')
+               ->with('mensagem.sucesso',"Série '{$serie->nome}' adicionada com sucesso");
    }
 
-   public function destroy(Request $request) {
-      Serie::destroy($request->series);
-      return redirect()->route('series.index');
+   public function destroy(Serie $series, Request $request) {
+      $series->delete();
+      // $request->session()->put('mensagem.sucesso', 'Série Removida com sucesso.');
+      // $request->session()->flash('mensagem.sucesso', "Série '{$series->nome}' Removida com sucesso.");
+      return redirect()
+               ->route('series.index')
+               ->with('mensagem.sucesso', "Série '{$series->nome}' Removida com sucesso.");
    }
 }
--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,5 +1,12 @@
 <x-layout title="Séries">
     <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
+
+    @isset($mensagemSucesso)
+    <div class="alert alert-success">
+      {{ $mensagemSucesso }}
+    </div>
+    @endisset
+
     <ul class="list-group">
       @foreach($series as $serie)
         <li class="list-group-item d-flex justify-content-between align-items-center">
