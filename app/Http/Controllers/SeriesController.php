d44def7fe36fd9e397179e7d881c296de9fbe5e6
Daniela Poletto <danielapoletto_bep@hotmail.com>
2023-01-26 20:53:49 -0300

    incluindo transações para salvar serie, temporada e episodios

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -30,38 +30,43 @@ class SeriesController extends Controller
 
    public function store(SeriesFormRequest $request) 
    {
-      $serie = Series::create($request->all());
-      // for ($i=1; $i <= $request->seasonsQty; $i++) { 
-      //    $season = $serie->seasons()->create([
-      //       'number' => $i
-      //    ]);
-
-      //    for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
-      //       $season->episodes()->create([
-      //          'number' => $j
-      //       ]);
-      //    }
-      // }
-      
-      $seasons = [];
-      for ($i=1; $i <= $request->seasonsQty; $i++) { 
-         $seasons[] = [
-            'series_id' => $serie->id,
-            'number' => $i,
-         ];
-      }
-      Season::insert($seasons);
-
-      $episodes = [];
-      foreach ($serie->seasons as $season) {
-         for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
-            $episodes[] = [
-               'season_id' => $season->id,
-               'number' => $j
+      $serie = null;
+      //executa tudo o que está dentro e commita no banco
+      DB::transaction(function () use ($request, &$serie){
+         $serie = Series::create($request->all());
+         
+         $seasons = [];
+         for ($i=1; $i <= $request->seasonsQty; $i++) { 
+            $seasons[] = [
+               'series_id' => $serie->id,
+               'number' => $i,
             ];
-         } 
-      }
-      Episode::insert($episodes);
+         }
+         Season::insert($seasons);
+   
+         $episodes = [];
+         foreach ($serie->seasons as $season) {
+            for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
+               $episodes[] = [
+                  'season_id' => $season->id,
+                  'number' => $j
+               ];
+            } 
+         }
+         Episode::insert($episodes);
+      });
+
+          // for ($i=1; $i <= $request->seasonsQty; $i++) { 
+         //    $season = $serie->seasons()->create([
+         //       'number' => $i
+         //    ]);
+   
+         //    for ($j=1; $j <= $request->episodesPerSeason; $j++) { 
+         //       $season->episodes()->create([
+         //          'number' => $j
+         //       ]);
+         //    }
+         // }
 
       return redirect()
                ->route('series.index')
